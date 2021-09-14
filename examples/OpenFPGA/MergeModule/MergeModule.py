cd48f5f81a1715acc5d4b397f0f4d74cf1639233
Ganesh Gore <goreganesh007@gmail.com>
2021-09-13 19:50:22 -0600

    Updated merge module example

--- examples/OpenFPGA/MergeModule/MergeModule.py
+++ examples/OpenFPGA/MergeModule/MergeModule.py
@@ -1,18 +1,61 @@
 import os
 from spydrnet.ir.port import Port
+from spydrnet.ir.instance import Instance
+from spydrnet.ir.definition import Definition
 from spydrnet.ir.innerpin import InnerPin
 from spydrnet.ir.outerpin import OuterPin
 import spydrnet as sdn
 from spydrnet.util import selection
 from spydrnet.util.selection import Selection
 from pprint import pprint
+import logging
 
-def MergeModules(topModule, modulelist, newModuleName="mergedModule"):
-    RenameMap = {}
-    newMod = topModule.library.create_definition(name=newModuleName)
+logger = logging.getLogger(__name__)
+FORMAT = "%(levelname)5s (%(lineno)4s) - %(message)s"
+
+
+
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+def MergeModules(topModule, modulelist, useDefinition=None, newModuleName=None):
+    """
+    topModule : Temporary argument to create new module definition,
+                It will be replaced, when this fucntion moves to library class
+    modulelist: List of modules to merge together
+    Pinmap:     Optional argument to provide pin rename mapping in case of conflict
+    useDefinition:     Optional argument, if provided instead of creating new module it will reuse this definition
+    newModuleName:  Optional argument, to give specific name to new module
+                    Default name: <moduel1>_<module2>...<module.>_merge
+    """
+
+    RenameMap = {} # Suppose to store pin rename map
+    IncomingWires = []
+
+    # Sanity checks
+    for eachModule in modulelist:
+        assert isinstance(eachModule, Instance), "Modulelist contains none non-intance object"
+
+    # Create a new module
+    if not useDefinition:
+        if not newModuleName:
+            newModuleName = "_".join([each.reference.name for each in modulelist]) + "_merged"
+            print(f"Inferred module name {newModuleName} ")
+        newMod = topModule.library.create_definition(name=newModuleName)
+    else:
+        assert isinstance(useDefinition, Definition), "useDefinition should be definition object"
+        newMod= useDefinition
+
+    # Create instance of the module
     MergedModule = topModule.create_child( name=f"{newModuleName}_1",
                     reference=newMod)
+
     for eachM in modulelist:
+        logger.info(f"******************************************************")
+        logger.info(f"Iterating module {eachM.name} [{eachM.reference.name}]")
+        logger.info(f"******************************************************")
+
+
+        # AddPorts
         RenameMap[eachM.name] = {}
         for p in eachM.get_ports():
             pClone = p.clone()
@@ -21,24 +64,32 @@ def MergeModules(topModule, modulelist, newModuleName="mergedModule"):
                 if not len(list(newMod.get_ports(newName))):
                     break
             pClone.name = newName
+
+            for eachPin in p.pins:
+                ClonedPin = eachPin.clone()
+                ClonedPin._wire = eachPin._wire.clone()
+                if eachPin.index() == 0:
+                    newCable = eachPin._wire.cable.clone()
+                    newCable.name = newName
+
+                ClonedPin._wire._cable = newCable
+                pClone.add_pin(ClonedPin,eachPin.index())
+                pClone.remove_pin(pClone._pins[-1])
+
             RenameMap[eachM.name][p.name] = newName
             newMod.add_port(pClone)
-            ww = newMod.create_cable(newName).create_wires(len(p.pins))
-            print(f"Adding port {pClone.name}")
 
-            for pin in p.get_pins(selection=selection.OUTSIDE):
-                print(pin.wire.index())
-                print(MergedModule.pins[pClone.pins[pin.wire.index()]])
-                pin.wire.connect_pin(MergedModule.pins[pClone.pins[pin.wire.index()]])
-                currWW = ww[pin.wire.index()]
-                inPin = eachM.pins[pin.inner_pin]
-                inPin.wire.disconnect_pin(inPin)
-                currWW.connect_pin(inPin)
+            for eachPin in p.pins:
+                instOutPin = eachM.pins[eachPin]
+                conWire = instOutPin.wire
+                instPin = MergedModule.pins[pClone.pins[eachPin.index()]]
+                conWire.connect_pin(instPin)
+                instOutPin.wire.disconnect_pin(instOutPin)
+                newCable.wires[eachPin.index()].connect_pin(instOutPin)
 
         topModule.remove_child(eachM)
         newMod.add_child(eachM)
-
-    pprint(RenameMap)
+    return RenameMap
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -59,3 +110,4 @@ sdn.compose(netlist, os.path.join(dir_path, "_result.v"))
 for eachDef in work.get_definitions():
     eachDef.name += "_eq"
 sdn.compose(netlist, os.path.join(dir_path, "_result_equiv.v"))
+logger.info("Finishing script")
