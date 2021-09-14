82f251acc1a1b5942c0ddbda1458a0761d8b24fc
Ganesh Gore <goreganesh007@gmail.com>
2021-09-13 19:28:07 -0600

    Updated rename port example

--- examples/OpenFPGA/PortPinRename/PortPinRename.py
+++ examples/OpenFPGA/PortPinRename/PortPinRename.py
@@ -15,8 +15,8 @@ or2 = next(topModule.get_instances("or2_1"))
 APort = next(or2.get_ports("A"))
 print(f"Current port name {APort.name}")
 
-next(APort.get_cables(APort.name)).name = "Ain"
-APort.name = "Ain"
+# next(APort.get_cables(APort.name)).name = "Ain"
+APort.change_name("Ain")
 
 print(f"Renamed port name {APort.name}")
 
