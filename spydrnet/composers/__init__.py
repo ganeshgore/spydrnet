0dafa86281efd564e8476b37e7fc96572413fa21
Ganesh Gore <ggore@quicklogic.com>
2024-03-11 20:03:25 -0600

    Updated verilog composer __init__

--- spydrnet/composers/__init__.py
+++ spydrnet/composers/__init__.py
@@ -7,24 +7,22 @@ from spydrnet.composers.eblif.eblif_composer import EBLIFComposer
 def compose(
     netlist,
     filename,
-    definition_list=[],
-    write_blackbox=True,
-    write_eblif_cname=True,
-    defparam=False,
+    *arg,
+    **kwargs
 ):
     """To compose a file into a netlist format"""
     extension = Path(filename).suffix
     extension_lower = extension.lower()
     if extension_lower in {".edf", ".edif"}:
-        composer = ComposeEdif()
+        composer = ComposeEdif(*arg, **kwargs)
         if netlist.name is None:
             raise Exception("netlist.name undefined")
         composer.run(netlist, filename)
     elif extension_lower in [".v", ".vh", ".vm"]:
-        composer = VerilogComposer(definition_list, write_blackbox, defparam)
+        composer = VerilogComposer(*arg, **kwargs)
         composer.run(netlist, file_out=filename)
     elif extension_lower in [".eblif", ".blif"]:
-        composer = EBLIFComposer(write_blackbox, write_eblif_cname)
+        composer = EBLIFComposer(*arg, **kwargs)
         composer.run(netlist, filename)
     else:
         raise RuntimeError("Extension {} not recognized.".format(extension))
